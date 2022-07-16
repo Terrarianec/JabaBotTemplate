77841e0a67761203ab175b1374b5821f6af575f6
Terrarianec#7870 <48604002+Terrarianec@users.noreply.github.com>
2022-07-16 20:03:35 +0300

    Исправлен загрузчик и обработчик команд
    Ошибки по невнимательности и из-за отсутствия тестирования

--- client.js
+++ client.js
@@ -31,13 +31,13 @@ function loadJSfilesFromDirectory(directory) {
 const commandFileNames = loadJSfilesFromDirectory(commandFolder),
     eventFileNames = loadJSfilesFromDirectory(eventFolder);
 
-for (let i = 0; i < commandFileNames; i++) { // https://learn.javascript.ru/array#perebor-elementov
+for (let i = 0; i < commandFileNames.length; i++) { // https://learn.javascript.ru/array#perebor-elementov
     const commandFileName = commandFileNames[i],
         commandFile = require(`${commandFolder}/${commandFileName}`), // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Template_literals#%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%BF%D0%BE%D0%BB%D1%8F%D1%86%D0%B8%D1%8F_%D0%B2%D1%8B%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B9
         commandData = commandFile.data.toJSON  // https://learn.javascript.ru/ifelse#uslovnyy-operator
             ? commandFile.data.toJSON() // Для SlashCommandBuilder
             : commandFile.data; // Для сырых данных
-    client.commands.set(commandData.name, commandFileName);
+    client.commands.set(commandData.name, commandFile);
 }
 
 for (let i = 0; i < eventFileNames.length; i++) {
--- events/_ready.js
+++ events/_ready.js
@@ -4,7 +4,7 @@ const guildId = "123456789012345678" // Вставить нужное
 module.exports = (client) => { // Альтернатива client.login(token).then(() => {...})
     console.log(`${client.user.tag} successfully logged in`);
     const commands = [];
-    client.commands.each(command => commands.push(command));
+    client.commands.each(command => commands.push(command.data));
 
     client.application.commands.set(commands, guildId); // Привязка к конкретной гильдии, если команда глобальная - стереть параметр идентификатора гильдии
 }
